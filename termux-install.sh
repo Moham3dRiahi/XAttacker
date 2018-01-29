bf04b175f0b8c9d8b19b5b54132396d179b096f1
Mohamed Riahi <Moham3driahi@gmail.com>
2018-01-29 22:13:30 +0100

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -14,4 +14,13 @@ perl Makefile.PL
 make
 make test
 make install
-cpan install Getopt::Long && cpan install HTTP::Request && cpan install LWP::UserAgent && cpan install IO::Select && cpan install HTTP::Cookies && cpan install HTTP::Response && cpan install Term::ANSIColor && cpan install URI::URL && cpan install IO::Socket::INET && cpan install Try::Tin
+cpan install Getopt::Long
+cpan install HTTP::Request
+cpan install LWP::UserAgent
+cpan install IO::Select
+cpan install HTTP::Cookies
+cpan install HTTP::Response
+cpan install Term::ANSIColor
+cpan install URI::URL
+cpan install IO::Socket::INET
+cpan install Try::Tin
