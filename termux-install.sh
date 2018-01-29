c5f10e7e39b2b5cf0e9dce8abd879c07d58862f9
Mohamed Riahi <Moham3driahi@gmail.com>
2018-01-29 22:04:22 +0100

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -1,24 +1,17 @@
-#!/bin/bash 
-clear 
-pkg install -y wget perl make 
-wget http://search.cpan.org/CPAN/authors/id/O/OA/OALDERS/HTTP-Message-6.14.tar.gz 
-tar zxf HTTP-Message-6.14.tar.gz 
-cd HTTP-Message-6.14 
-perl Makefile.PL 
-make 
-make test 
-make install 
-wget http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/libwww-perl-6.31.tar.gz 
-tar zxf libwww-perl-6.31.tar.gz 
-cd libwww-perl-6.31 
-perl Makefile.PL 
-make 
-make test 
-make install 
-cpan install Getopt::Long && cpan install HTTP::Request && cpan install LWP::UserAgent && cpan install IO::Select && cpan install HTTP::Cookies && cpan install HTTP::Response && cpan install Term::ANSIColor && cpan install URI::URL && cpan install IO::Socket::INET && cpan install Try::Tin 
-clear 
-echo "[√] Tool installed successfully [√]"; 
-echo ""; 
-echo " All is done "; 
-echo " You can run tool by typing "; 
-echo " perl XAttacker.pl ";
+#!/bin/bash 
+pkg install -y wget perl make
+wget http://search.cpan.org/CPAN/authors/id/O/OA/OALDERS/HTTP-Message-6.14.tar.gz
+tar zxf HTTP-Message-6.14.tar.gz
+cd HTTP-Message-6.14
+perl Makefile.PL
+make
+make test
+make install
+wget http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/libwww-perl-6.31.tar.gz
+tar zxf libwww-perl-6.31.tar.gz
+cd libwww-perl-6.31
+perl Makefile.PL
+make
+make test
+make install
+cpan install Getopt::Long && cpan install HTTP::Request && cpan install LWP::UserAgent && cpan install IO::Select && cpan install HTTP::Cookies && cpan install HTTP::Response && cpan install Term::ANSIColor && cpan install URI::URL && cpan install IO::Socket::INET && cpan install Try::Tin
