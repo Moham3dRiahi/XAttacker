5372f9fc5d23c6e0e8d0c38a3d36f5fb9de17961
Mohamed Riahi <Moham3driahi@gmail.com>
2018-01-30 01:35:52 +0100

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -2,18 +2,10 @@
 pkg install -y perl make
 wget http://search.cpan.org/CPAN/authors/id/O/OA/OALDERS/HTTP-Message-6.14.tar.gz
 tar zxf HTTP-Message-6.14.tar.gz
-cd HTTP-Message-6.14/
-perl Makefile.PL
-make
-make test
-make install
+cd HTTP-Message-6.14/ && perl Makefile.PL && make && make test && make install
 wget http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/libwww-perl-6.31.tar.gz
 tar zxf libwww-perl-6.31.tar.gz
-cd libwww-perl-6.31/
-perl Makefile.PL
-make
-make test
-make install
+cd libwww-perl-6.31/ && perl Makefile.PL && make && make test && make install
 cpan install Getopt::Long
 cpan install HTTP::Request
 cpan install LWP::UserAgent
