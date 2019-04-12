af1fa14277c399db3701bf00f295395194593624
Yusuf Abdulla Shunan <shunan@gmail.com>
2019-04-12 16:04:53 +0500

    termux deps fix

--- termux-install.sh
+++ termux-install.sh
@@ -1,12 +1,22 @@
-pkg install -y perl make
-wget http://search.cpan.org/CPAN/authors/id/O/OA/OALDERS/HTTP-Message-6.14.tar.gz
+pkg upgrade -y
+pkg install -y perl
+pkg install -y make
+pkg install -y clang
+pkg install -y curl
+pkg install -y wget
+pkg install -y lftp
+cpan install CPAN
+cpan reload
+cpan install local::lib
+cpan install App::Log4perl
+wget http://search.cpan.org/CPAN/authors/id/O/OA/OALDERS/HT
 tar zxf HTTP-Message-6.14.tar.gz
 cd HTTP-Message-6.14/
 perl Makefile.PL
 make
 make test
 make install
-wget http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/libwww-perl-6.31.tar.gz
+wget http://search.cpan.org/CPAN/authors/id/E/ET/ETHER/libw
 tar zxf libwww-perl-6.31.tar.gz
 cd libwww-perl-6.31/
 perl Makefile.PL
@@ -23,5 +33,5 @@ cpan install Term::ANSIColor
 cpan install URI::URL
 cpan install IO::Socket::INET
 cpan install Try::Tiny
-echo "[✔] All is done! [✔]";
+echo "All ok";
 echo "[✔] Now You can run tool [✔]";
